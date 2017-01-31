92af29f0ce749d2c02c78a598ea8431ef3240062
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-01-30 22:31:24 -0600

    Added constructor to Node class

--- search_agents_and_algorithms/Node.py
+++ search_agents_and_algorithms/Node.py
@@ -6,9 +6,9 @@ Created on Mon Jan 30 22:14:33 2017
 """
 
 class Node:
-
-    def __init__(self):
-        self.parent = Node()
-        self.path_cost = 0.0
-        self.visited = False
-        self.stage = []
+        
+    def __init__(self,parent=None,path_cost = 0.0,visited=False,state=[]):
+        self.parent = parent
+        self.path_cost = path_cost
+        self.visited = visited
+        self.state = state
