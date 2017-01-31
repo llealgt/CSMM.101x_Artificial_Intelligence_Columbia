68ec0d9034597bd4ef476f0c5097b2da4beb4e91
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-01-30 23:08:32 -0600

    Added board validation logic to driver program, and constructor to Node class

--- search_agents_and_algorithms/driver.py
+++ search_agents_and_algorithms/driver.py
@@ -1,8 +1,33 @@
 import sys
+import math
+from Node import Node
 
 if __name__ == '__main__':
-    method = sys.argv[1]
-    board = sys.argv[2]
     
-    print(method)
-    print(board)
+    #input search algorithm and initial state array
+    method = sys.argv[1]
+    board = map(int,sys.argv[2].split(','))
+    
+    #Validate initial board(is square if square root is integer)
+    #End execution if initial board is not valid
+    board_size = len(board)
+
+    if not math.sqrt(board_size).is_integer():
+        print("Invalid board,try again")
+        sys.exit(0)
+    
+    #create the root node with the given board(initial state)
+    root_node =  Node(state=board)
+    
+    #perform the selected method
+    if method == 'bfs':
+        print('bfs')
+    elif method == 'dfs':
+        print('dfs')
+    elif method == 'ast':
+        print('ast')
+    elif method == 'ida':
+        print('ida')
+    else:
+        print('Not valid method selected')
+    
