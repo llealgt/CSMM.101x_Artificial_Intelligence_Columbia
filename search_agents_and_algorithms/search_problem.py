3095f41bd818f024cf26e4de217217d41bfe222f
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-02-19 21:08:58 -0600

    Finished child_node function that returns and node given a parent node an an action in that node

--- search_agents_and_algorithms/search_problem.py
+++ search_agents_and_algorithms/search_problem.py
@@ -13,7 +13,7 @@ class SearchProblem:
     
     def __init__(self,initialState = []):
         self.rootNode = Node(state = initialState )
-        self.size = Math.sqrt(len(initialState))
+        self.size = int(math.sqrt(len(initialState)))
 
     def breadth_first_search(self):
         print("bfs")
@@ -62,30 +62,40 @@ class SearchProblem:
     def search_template(self,algorithm ="bfs"):
         print("Create a generic search")
         
-        
+    #Return a node representing the result of an action in a parent node    
     def child_node(self,parent,action):
         
         #get the current location of the "0" to verify if is a valid action
         zero_row = self.get_element_row(parent.zero_position)
         zero_col = self.get_element_col(parent.zero_position)
-        #if its a non permited operation, return None
-        state = parent.state
+        #get a copy(because it is handled by reference, not value) of the state that the parent node represents
+        state = list(parent.state)
 
+        #we need to swap the empty tide(zero value) with another tide depending on the action
+        #we will validate swaps(if not valid return None) and calculate swap indexes depending 
+        #on current indexes and action
         if action == "up" and zero_row > 0:
-            print "up"
+            row_index = -1
+            col_index = 0
         elif action == "down" and zero_row< self.size-1:
-            print "down"
-            tmp_value = state[(zero_row+1)*self.size + zero_col]
-            state[(zero_row+1)*self.size + zero_col] = state[(zero_row)*self.size + zero_col]
-            state[(zero_row)*self.size + zero_col] = tmp_value
+            row_index = 1
+            col_index = 0
         elif action == "right" and zero_col < self.size -1:
-            print "right"
+            row_index = 0
+            col_index = 1
         elif action == "left" and zero_col > 0:
-            print "left"
+            row_index = 0
+            col_index = -1
         else:
+            #if its a non permited operation, return None
             return None
             
-        child = Node(parent = parent, state = state)
+        #swap empty(0) tide with corresponding adjacent tide    
+        temp_value = state[(zero_row+row_index)*self.size + zero_col + col_index]
+        state[(zero_row+row_index)*self.size + zero_col + col_index] = 0
+        state[zero_row*self.size + zero_col] = temp_value
+            
+        child = Node(parent = parent, state = state ,zero_position=(zero_row+row_index)*self.size + zero_col + col_index)
         
         return child
         
